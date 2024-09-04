34499b8249cf81ca8f908d89efcfb42d2038bc5c
7Pushpraj <117337991+7Pushpraj@users.noreply.github.com>
2024-09-04 19:44:49 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+  button.addEventListener('click', function(e){
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case 'grey':body.style.backgroundColor = e.target.id;
+        break;
+        
+        case 'white':body.style.backgroundColor = e.target.id;
+        break;
+
+        case 'blue':body.style.backgroundColor = e.target.id;
+        break;
+
+        case 'yellow':body.style.backgroundColor = e.target.id;
+        break;
+
+        default:
+          console.log("nomatching color found for id: " + e.target.id);
+    }
+  });
+});
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-by-chaiaurcode
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/7Pushpraj/dom-project-by-chaiaurcode)
